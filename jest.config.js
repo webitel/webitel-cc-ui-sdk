f938a2e3d0296cc5e66cca9a20a52568f7c4c10d
dlohvinov <nsblnr@gmail.com>
2022-02-02 13:33:25 +0200

    0.1.18: upd webitel-ui to 1.0.1

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transformIgnorePatterns: [
-    '/node_modules/(?!@webitel/ui-sdk/src|webitel-sdk).+\\.js$',
+    // '/node_modules/(?!@webitel/ui-sdk/src|webitel-sdk).+\\.js$',
   ],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/unit/mocks/fileMock.js',
